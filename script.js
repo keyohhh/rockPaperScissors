ee3176d3a724b523a7855852d599c6b28cad5ba6
Keough <132158961+keyohhh@users.noreply.github.com>
2024-03-13 12:53:22 -0400

    finished rock paper scissor game

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-// initialize Rock paper scissors
-// Get player choice - Rock, Paper, or Scissors
-// Get random computer choice - Rock, Paper, or Scissors
-// Get computer choice and player choice to compare them
-// Take choice and print winner
-
 const answers = ["rock", "paper", "scissor"];
 
-// get random computer choice
 function getComputerChoice() {
     let index = Math.floor(Math.random() * answers.length);
     return answers[index];
 }
 
-//get players choice
 function getPlayerChoice() {
     let pick = prompt(answers);
     return pick;
 }
 
-// compare and declare winner
 function playRound() {
     const playerSelection = getPlayerChoice();
     let computerSelection =  getComputerChoice();
     if (playerSelection === computerSelection) {
         console.log("Round ends in a draw!")
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-        console.log("Player Wins this round rock beats paper!")
+        console.log(`Computer Wins this round ${computerSelection} beats ${playerSelection}!`)
+    } else if (computerSelection === "rock" && playerSelection === "paper") {
+        console.log(`Player wins this round ${playerSelection} beats ${computerSelection}!`)
+    } else if (playerSelection === "rock" && computerSelection === "scissor") {
+        console.log(`Player wins this round ${playerSelection} beats ${computerSelection}!`)
+    } else if (computerSelection === "rock" && playerSelection === "scissor") {
+        console.log(`Computer wins this round ${computerSelection} beats ${playerSelection}!`)
+    } else if (playerSelection === "paper" && computerSelection === "scissor") {
+        console.log(`Computer wins this round ${computerSelection} beats ${playerSelection}!`)
+    } else if (computerSelection === "paper" && playerSelection === "scissor") {
+        console.log(`Player wins this round ${playerSelection} beats ${computerSelection}!`)
     }
 }
 
