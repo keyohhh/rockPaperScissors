b8bfc91350ca208fd3d38696e101eb115828c7fe
Keough <132158961+keyohhh@users.noreply.github.com>
2024-03-26 08:04:36 -0400

    finished RPS game

--- script.js
+++ script.js
@@ -1,37 +1,107 @@
-const answers = ["rock", "paper", "scissor"];
+const rockBtn = document.querySelector(".rock-btn");
+const paperBtn = document.querySelector(".paper-btn");
+const scissorsBtn = document.querySelector(".scissors-btn");
+const playerScore = document.querySelector(".pscore");
+const computerScore = document.querySelector('.cscore');
+const startGame = document.querySelector('.start')
+const results = document.querySelector('.results')
 
-function getComputerChoice() {
-    let index = Math.floor(Math.random() * answers.length);
-    return answers[index];
+let pscore = "0";
+let cscore = "0";
+let gameInProgress;
+let playerChoice = "";
+
+function computerChoice() {
+    const answers = ["rock", " paper", " scissors"];
+    let choice = Math.floor(Math.random() * answers.length);
+    return choice;
 }
 
-function getPlayerChoice() {
-    let pick = prompt(answers);
-    return pick;
-}
+startGame.addEventListener("click", () => {
+    startGame.style.visibility = 'hidden'
+    playerScore.textContent = pscore;
+    computerScore.textContent = cscore;
+    gameInProgress = true;
+})
 
-function playRound() {
-    const playerSelection = getPlayerChoice();
-    let computerSelection =  getComputerChoice();
-    if (playerSelection === computerSelection) {
-        console.log("Round ends in a draw!")
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        console.log(`Computer Wins this round ${computerSelection} beats ${playerSelection}!`)
-    } else if (computerSelection === "rock" && playerSelection === "paper") {
-        console.log(`Player wins this round ${playerSelection} beats ${computerSelection}!`)
-    } else if (playerSelection === "rock" && computerSelection === "scissor") {
-        console.log(`Player wins this round ${playerSelection} beats ${computerSelection}!`)
-    } else if (computerSelection === "rock" && playerSelection === "scissor") {
-        console.log(`Computer wins this round ${computerSelection} beats ${playerSelection}!`)
-    } else if (playerSelection === "paper" && computerSelection === "scissor") {
-        console.log(`Computer wins this round ${computerSelection} beats ${playerSelection}!`)
-    } else if (computerSelection === "paper" && playerSelection === "scissor") {
-        console.log(`Player wins this round ${playerSelection} beats ${computerSelection}!`)
+rockBtn.addEventListener("click", () => {
+    if (gameInProgress) {
+        playerChoice = "rock";
+        playRound(playerChoice, computerChoice())
+    }
+})
+
+paperBtn.addEventListener("click", () => {
+    if (gameInProgress) {
+        playerChoice = "paper";
+        playRound(playerChoice, computerChoice())
+    }
+})
+
+scissorsBtn.addEventListener("click", () => {
+    if (gameInProgress) {
+        playerChoice = "scissors"
+        playRound(playerChoice, computerChoice())
+    }
+})
+
+function fiveRounds() {
+    if (playerScore === 5) {
+        gameInProgress = false;
+        alert("play Again")
     }
 }
 
-function playGame() {
-  /*  for (let i = 0; i < 5; i++) {        //Logic for 5 games, ON HOLD.
-        playRound();
-    }   */
-}
+function playRound(playerChoice, computerChoice){
+    if (playerChoice === "rock"){
+        switch (computerChoice){
+            case 0: {
+                return "it's a draw";
+            }
+            case 1: {
+                cscore++;
+                computerScore.textContent = cscore;
+                return "you lost, paper beats rock";
+                
+            }
+            case 2: {
+                pscore++;
+                playerScore.textContent = pscore;
+                return "you won, rock beats scissor"
+            }
+        }
+    } else if (playerChoice === "scissors"){
+        switch (computerChoice){
+            case 0: {
+                cscore++;
+                computerScore.textContent = cscore;
+                return "you lost, rock beats scissor";
+            }
+            case 1: {
+                pscore++;
+                playerScore.textContent = pscore;
+                return "you won, scissor beats paper";
+            }
+            case 2: {
+                return "it's a draw"
+            }
+        }
+    } else if (playerChoice === "paper"){
+        switch (computerChoice){
+            case 0:{
+                pscore++;
+                playerScore.textContent = pscore;
+                return "you won, paper beats rock";
+            }
+            case 1:{
+                return "it's a draw";
+            }
+            case 2:{
+                cscore++;
+                computerScore.textContent = cscore;
+                return "you lost, scissor beats paper"
+            }
+        }
+    }    
+    }
+
